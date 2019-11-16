59c97b0ba3324c3a7775e043c16cd50a438e13e0
Alex <alexw.f@hotmail.com>
2019-11-15 22:00:13 -0300

    arquivos

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace fila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+             
+            //Inserindo três elementos
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            
+            Console.WriteLine("Listando elementos da fila:");
+            
+            //Enquanto houver elementos na lista, exibir e remover o primeiro
+            while (q.Count > 0)
+            {
+                Console.WriteLine(q.Dequeue());
+            }
+            
+            //Exibe a quantidade de elementos restantes, ou seja, zero
+            Console.WriteLine("A lista agora possui " + q.Count.ToString() + " elementos.");
+            Console.Read();
+        }
+    }
+}
